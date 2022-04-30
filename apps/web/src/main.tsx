51ba3428b75459dd95bad675fed772475a4cd24a
Eugeniu Megherea <eugeniu.megherea@vitamojo.com>
2022-04-30 12:42:11 +0300

    wip

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,31 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import App from './app/app';
+import { theme } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {/* <App /> */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/app" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+
+
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
